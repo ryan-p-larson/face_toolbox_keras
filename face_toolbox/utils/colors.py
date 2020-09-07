cbda8570e18906df3fbb100f0dcc48e8d2081e42
Ryan Larson <ryalarson11@gmail.com>
2020-09-07 17:22:02 -0500

    add updated color map for segments

---
+++ face_toolbox/utils/colors.py
@@ -0,0 +1,45 @@
+
+
+OLD_SEGMENT_CMAP = [
+  [ 0, '#ffffff'], # background
+  [ 1, '#a1d99b'], # skin
+  [ 2, '#41ab5d'], # eyebrow
+  [ 3, '#41ab5d'], # eyebrow
+  [ 4, '#238b45'], # eye
+  [ 5, '#238b45'], # eye
+  [ 6, '#6edf00'], # glasses
+  [ 7, '#00fb75'], # ear
+  [ 8, '#00fb75'], # ear
+  [ 9, '#6edf00'], # earings
+  [10, '#c7e9c0'], # nose
+  [11, '#F0AA99'], # mouth
+  [12, '#F0AA99'], # mouth
+  [13, '#F0AA99'], # mouth
+  [14, '#005718'], # neck
+  [15, '#005718'], # neck_lower
+  [16, '#decbe4'], # cloth
+  [17, '#e5d8bd'], # hair
+  [18, '#ffea00']  # hat
+]
+
+INFERNO_SEGMENT_CMAP = [
+  [ 0, '#ffffff'], # background
+  [ 1, '#003f5c'], # skin
+  [ 2, '#444e86'], # eyebrow
+  [ 3, '#444e86'], # eyebrow
+  [ 4, '#444e86'], # eye
+  [ 5, '#444e86'], # eye
+  [ 6, '#444e86'], # glasses
+  [ 7, '#955196'], # ear
+  [ 8, '#955196'], # ear
+  [ 9, '#955196'], # earings
+  [10, '#955196'], # nose
+  [11, '#F0AA99'], # mouth
+  [12, '#F0AA99'], # mouth
+  [13, '#F0AA99'], # mouth
+  [14, '#dd5182'], # neck
+  [15, '#dd5182'], # neck_lower
+  [16, '#ff6e54'], # cloth
+  [17, '#ff6e54'], # hair
+  [18, '#ffa600']  # hat
+]
